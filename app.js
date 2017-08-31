c2e1f339ed4a38b7ef2c027b70a369d56456f3d9
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-08-31 15:40:30 -0500

    Adds component for google map with marker

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
     $('select').material_select();
 });
+
+function initMap() {
+  var austin = {lat: 30.2672, lng: -97.7431};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: austin
+  });
+  var marker = new google.maps.Marker({
+    position: austin,
+    map: map
+  });
+}
