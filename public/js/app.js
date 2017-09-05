9466c043f12b01b08fd20e3da64a39356078ba52
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-09-04 19:37:49 -0500

    Adds markers and info window for marker details

--- public/js/app.js
+++ public/js/app.js
@@ -4,15 +4,22 @@ $(document).ready(function() {
 
 });
 
+
+// MOCK DATA
+// TODO replace with real crime objects - lat, lng, crime type
 let locations = [
-  {lat: 30.3312282, lng: -97.62328346},
-  {lat: 30.38949131, lng: -97.64854574},
-  {lat: 30.30960673, lng: -97.65506085},
-  {lat: 30.37243668, lng: -97.66574384}
+  {location: {lat: 30.3312282, lng: -97.62328346}, crimeType: 'ROBBERY BY ASSAULT', date: '06/04/2016'},
+  {location: {lat: 30.38949131, lng: -97.64854574}, crimeType:'HARASSMENT', date: '02/06/2016'},
+  {location: {lat: 30.30960673, lng: -97.65506085}, crimeType:'POSS CONTROLLED SUB/NARCOT', date: '08/08/2016'},
+  {location: {lat: 30.37243668, lng: -97.66574384}, crimeType:'PUBLIC INTOXICATION', date: '08/06/2016'}
 ];
 
 
-let map;
+let map = null;
+
+// intialize blank array for all crimes markers
+let markers = [];
+
 
 // instantiance map object with default properties
 function initMap() {
@@ -20,16 +27,55 @@ function initMap() {
   // set high level zoom on austin - higher zoom number is lower to the ground
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
-    center: austin
+    center: AUSTIN
   });
 
+  // initialize infowindow, and map boundaries
+  let largeInfowindow = new google.maps.InfoWindow();
+  let bounds = new google.maps.LatLngBounds();
 
-  // var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  // loop through locations array aka seed data to create markers
-  let markers = locations.map((location, i) => {
-    return new google.maps.Marker({
-      position: location,
-      // label: labels[i % labels.length]
+  // loop through locations array to create markers for crimes on initialization
+  for (var i = 0; i < locations.length; i++) {
+    let position = locations[i].location;
+    let title = locations[i].crimeType;
+
+    // create a new marker for each location
+    let marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i
     });
-  });
+    // add to markers array
+    markers.push(marker);
+
+    // extend map boundaries for each marker
+    bounds.extend(marker.position);
+
+    // add listeners to open infowindow with crime details on click
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
+  }
+  // update map to new boundaries
+  map.fitBounds(bounds);
+
+}
+
+// populates infowindow when a marker is clicked
+// only one infowindow allowed open at a time
+// data is populated based on markers position
+function populateInfoWindow(marker, infowindow) {
+  // check that the infowindow is not already opened on this marker
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent('<div>' + marker.title + '</div>');
+    infowindow.open(map, marker);
+
+    // clear marker property if an infowindow is closed
+    infowindow.addListener('closeclick', function() {
+      infowindow.marker = null;
+    });
+  }
 }
